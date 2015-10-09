8f3d76bde40378f14a63e8e87cdccabd7c5744b9
minhlunso1 <aminh1603@gmail.com>
2015-10-09 10:38:31 +0700

    Update GoCostActivity.java

--- app/src/main/java/com/example/minh/tryandroiddatabinding/view/GoCostActivity.java
+++ app/src/main/java/com/example/minh/tryandroiddatabinding/view/GoCostActivity.java
@@ -3,8 +3,6 @@ package com.example.minh.tryandroiddatabinding.view;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import com.example.minh.tryandroiddatabinding.R;
 import com.example.minh.tryandroiddatabinding.databinding.GoCostActivityBinding;
 import com.example.minh.tryandroiddatabinding.model.GoCost;
@@ -22,38 +20,5 @@ public class GoCostActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.go_cost_activity);
         final GoCostViewModel goCostViewModel = new GoCostViewModel(new GoCost());
         binding.setGoCostVM(goCostViewModel);
-
-        binding.etDistance.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                goCostViewModel.setCost(s.toString(), binding.etUnit.getText().toString());
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-        binding.etUnit.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                goCostViewModel.setCost(binding.etDistance.getText().toString(), s.toString());
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
     }
 }
